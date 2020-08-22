3acf3b232844e8f1518057a195d9cec2264570b9
wilembergson <wilembergson.yahoo.com>
2020-08-22 20:15:30 -0300

    Testando a primeira classe de dominio - Categoria

--- src/main/java/com/wilembergson/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/wilembergson/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.wilembergson.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.wilembergson.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST esta funcionando!";
+	public List<Categoria> listar() {
+		
+		Categoria catt1 = new Categoria(1, "Informatica");
+		Categoria catt2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> lista =  new ArrayList<>();
+		lista.add(catt1);
+		lista.add(catt2);
+		
+		return lista;
 	}
 }
