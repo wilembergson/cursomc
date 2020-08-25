c0086c9c59332fe2f9db04fc0bbd2056850ee597
wilembergson <wilembergson.yahoo.com>
2020-08-24 22:16:34 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/wilembergson/cursomc/services/CategoriaService.java
+++ src/main/java/com/wilembergson/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.wilembergson.cursomc.domain.Categoria;
 import com.wilembergson.cursomc.repositories.CategoriaRepository;
+import com.wilembergson.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -15,7 +16,8 @@ public class CategoriaService {
 	private CategoriaRepository repo;
 	
 	public Categoria buscar(Integer id) {
-		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		Optional<Categoria> obj = repo.findById(id); 
+		return obj.orElseThrow(() -> new ObjectNotFoundException( "Objeto não encontrado! Id: " + id 
+				+ ", Tipo: " + Categoria.class.getName()));	
 	}
 }
