342ceb058cc1993fdbf590e74ab099af09e8808e
wilembergson <wilembergson.yahoo.com>
2020-08-27 13:30:33 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/wilembergson/cursomc/domain/Cliente.java
+++ src/main/java/com/wilembergson/cursomc/domain/Cliente.java
@@ -38,6 +38,9 @@ public class Cliente implements Serializable{
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
+	@OneToMany(mappedBy="cliente")
+	private List<Pedido> pedido = new ArrayList<>();
+	
 	public Cliente() {
 	}
 
@@ -106,6 +109,14 @@ public class Cliente implements Serializable{
 		this.telefones = telefones;
 	}
 
+	public List<Pedido> getPedido() {
+		return pedido;
+	}
+
+	public void setPedido(List<Pedido> pedido) {
+		this.pedido = pedido;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -130,6 +141,5 @@ public class Cliente implements Serializable{
 			return false;
 		return true;
 	}
-	
-	
+
 }
