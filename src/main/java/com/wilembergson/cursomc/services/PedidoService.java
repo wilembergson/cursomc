ec63525b301cb3e914c2614ae93527c6195d4219
wilembergson <wilembergson.yahoo.com>
2020-08-28 13:19:17 -0300

    Endpoint /pedidos/{id} disponibilizados

---
+++ src/main/java/com/wilembergson/cursomc/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.wilembergson.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wilembergson.cursomc.domain.Pedido;
+import com.wilembergson.cursomc.repositories.PedidoRepository;
+import com.wilembergson.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id); 
+		return obj.orElseThrow(() -> new ObjectNotFoundException( "Objeto não encontrado! Id: " + id 
+				+ ", Tipo: " + Pedido.class.getName()));	
+	}
+}
