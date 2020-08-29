46c66510b8ffb29bc56acfe62fe9abae3b573bd4
wilembergson <wilembergson.yahoo.com>
2020-08-29 20:16:19 -0300

    Deletando Categorias com DELETE

--- src/main/java/com/wilembergson/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/wilembergson/cursomc/resources/CategoriaResource.java
@@ -41,4 +41,9 @@ public class CategoriaResource {
 		return ResponseEntity.noContent().build();
 	}
 
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Categoria> delete(@PathVariable Integer id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 }
--- src/main/java/com/wilembergson/cursomc/services/CategoriaService.java
+++ src/main/java/com/wilembergson/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.wilembergson.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.wilembergson.cursomc.domain.Categoria;
 import com.wilembergson.cursomc.repositories.CategoriaRepository;
+import com.wilembergson.cursomc.services.exceptions.DataIntegrityException;
 import com.wilembergson.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,6 +31,17 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos.");
+		}
+		
 	}
 	
 }
