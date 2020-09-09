3c250fe4aceb7cf0d99db610c9729f4207322cf0
wilembergson <wilembergson.yahoo.com>
2020-09-09 13:56:11 -0300

    Criando profile de desenvolvimento.

---
+++ src/main/java/com/wilembergson/cursomc/config/DevConfig.java
@@ -0,0 +1,31 @@
+package com.wilembergson.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.wilembergson.cursomc.services.DBServices;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	public DBServices dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		if(!"create".equals(strategy)) {
+			return false;
+		}
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+}
