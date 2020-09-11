bcdfc70f0b102f5c7418544a890c1501567ab52d
wilembergson <wilembergson.yahoo.com>
2020-09-11 20:35:39 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/wilembergson/cursomc/config/DevConfig.java
+++ src/main/java/com/wilembergson/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.wilembergson.cursomc.services.DBServices;
+import com.wilembergson.cursomc.services.EmailService;
+import com.wilembergson.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -27,5 +29,10 @@ public class DevConfig {
 		}
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 }
