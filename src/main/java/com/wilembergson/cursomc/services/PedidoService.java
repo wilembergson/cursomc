7635eb05a564fb5cf56507b7057c7ac47b4f47a8
wilembergson <wilembergson.yahoo.com>
2020-09-20 14:00:06 -0300

    Restrição de conteúdo: cliente só recupera seu pedidos.

--- src/main/java/com/wilembergson/cursomc/services/PedidoService.java
+++ src/main/java/com/wilembergson/cursomc/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.wilembergson.cursomc.domain.Cliente;
 import com.wilembergson.cursomc.domain.ItemPedido;
 import com.wilembergson.cursomc.domain.PagamentoComBoleto;
 import com.wilembergson.cursomc.domain.Pedido;
@@ -14,6 +18,8 @@ import com.wilembergson.cursomc.domain.enums.EstadoPagamento;
 import com.wilembergson.cursomc.repositories.ItemPedidoRepository;
 import com.wilembergson.cursomc.repositories.PagamentoRepository;
 import com.wilembergson.cursomc.repositories.PedidoRepository;
+import com.wilembergson.cursomc.security.UserSS;
+import com.wilembergson.cursomc.services.exceptions.AuthorizationException;
 import com.wilembergson.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -68,5 +74,15 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente =  clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 }
